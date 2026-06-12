07b35e804f6dd9dd30ebdb2a38e8b915768be917
Rohityadavv324 <136092626+Rohityadavv324@users.noreply.github.com>
2026-06-12 12:16:29 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,String> hashmap = new HashMap();
+        for(int i=0;i<nums.length;i++){
+            if(hashmap.put(nums[i],"Revalue")=="Revalue"){
+                return true;
+            }
+        }
+        return false;
+    }
+}
